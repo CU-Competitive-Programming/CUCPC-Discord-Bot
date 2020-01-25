711ff67fcc0da57cfb6bcd862dc7e0f72978a995
Derek Thomas <deth4407@colorado.edu>
2020-01-25 15:00:12 -0700

    db setting changes

--- handler/settings.py
+++ handler/settings.py
@@ -20,22 +20,15 @@ db = mongo["tank-game"]
 s = db.settings.find_one({})
 
 serverName = s.get("server")  # Name of server : str
-brackets = s.get("brackets")  # File that contains the image of brackets : str
 matches = s.get("matches")  # Scheduled matches : dict with str keys and values str
 logFile = s.get("log")  # File used for the logging : str
-season = s.get("season")  # Number of season : str
 token = s.get("token")  # Token for the discord bot : str
-submit = s.get("submit")  # Submissions opened or closed : bool
-onTour = s.get("onTour")  # If a tournament is ongoing : bool
-timeSub = s.get("timeSub")  # Time when submissions open
 compileOut = s.get("compileOut")  # Timeout for compiler
 admins = s.get("admins")  # The usernames of the admins
 specs = s.get("specs")  # Tournament specs
 engineLink = s.get("engineLink")  # Link with info about engine
 handlerUser = s.get("handlerUser")  # Username of the user that runs enviroment handler
 engine = s.get("engine")  # Engine information
-emojis = s.get("emojis")  # Emojis
-opened = s.get("open")  # Check if submissions are opened for everyone
 signup = s.get("signup")  # Get invite link
 
 g = db.game.find_one({})  # Get game settings
